c0c68f42b0fe75e114ae5b653570626c6ab2bba2
maxvaro1020 <maxvaro1020@gmail.com>
2026-07-14 11:13:20 -0600

    cambios que hice antes de haber actualizado

---
+++ script/scrip.js
@@ -0,0 +1,10 @@
+
+console.log("LEGACY JERSEYS");
+console.log("JavaScript externo cargado correctamente.");
+console.log("Bienvenido a la tienda.");
+
+
+
+window.onload = function () {
+    alert("¡Bienvenido a LEGACY JERSEYS!");
+};
