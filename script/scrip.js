2c692b0907e87a6a7a0d3f67e0a2a55e2100872b
maxvaro1020 <maxvaro1020@gmail.com>
2026-07-16 14:30:55 -0600

    Explicacion breve de lo que cambio o agrego

--- script/scrip.js
+++ script/scrip.js
@@ -3,8 +3,9 @@ console.log("LEGACY JERSEYS");
 console.log("JavaScript externo cargado correctamente.");
 console.log("Bienvenido a la tienda.");
 
-
-
 window.onload = function () {
-    alert("¡Bienvenido a LEGACY JERSEYS!");
+    if (!sessionStorage.getItem("bienvenidaMostrada")) {
+        alert("¡Bienvenido a LEGACY JERSEYS!");
+        sessionStorage.setItem("bienvenidaMostrada", "true");
+    }
 };
